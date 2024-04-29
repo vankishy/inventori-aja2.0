7f27cf4625b2d2318792f91f594987c489f94c5c
Reinhard Situmeang <101586289+vankishy@users.noreply.github.com>
2024-04-29 13:10:58 +0700

    add: loginUI

---
+++ src/inventoriaja/DriverMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventoriaja;
+
+/**
+ *
+ * @author SWIFT
+ */
+public class DriverMain {
+    public static void main(String[] args) {
+        var login = new LoginUI();
+        login.setVisible(true);
+    }
+}
